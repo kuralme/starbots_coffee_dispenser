c0f7fc2d37391b026987f7758fbac66899e502ba
kuralme <kuralme@protonmail.com>
2025-08-16 06:01:22 +0000

    sim moveit config added

---
+++ ur3e_sim_moveit_config/launch/move_group.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("name", package_name="ur3e_sim_moveit_config").to_moveit_configs()
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"trajectory_execution.allowed_execution_duration_scaling": 2.0,},
+            {"publish_robot_description_semantic": True},
+            {"use_sim_time": True},
+        ],
+    )
+    return LaunchDescription(
+        [move_group_node]
+    )
