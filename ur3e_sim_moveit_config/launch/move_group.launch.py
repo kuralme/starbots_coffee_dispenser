4dd9a95558ccef1284a9814413decd76a2870cad
kuralme <kuralme@protonmail.com>
2025-08-25 22:05:42 +0000

    sensor topics added for octomap - table topic needs to be filtered

--- ur3e_sim_moveit_config/launch/move_group.launch.py
+++ ur3e_sim_moveit_config/launch/move_group.launch.py
@@ -1,9 +1,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("name", package_name="ur3e_sim_moveit_config").to_moveit_configs()
+
+    moveit_config = MoveItConfigsBuilder("name", package_name="ur3e_sim_moveit_config").sensors_3d(
+            file_path=os.path.join(
+                get_package_share_directory("ur3e_sim_moveit_config"),
+                "config/sensors_3d.yaml",
+            )
+        ).to_moveit_configs()
+        
     move_group_node = Node(
         package="moveit_ros_move_group",
         executable="move_group",
