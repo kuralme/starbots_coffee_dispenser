1e6949d2057705b9cd0b166ef39fb27a64dae4f5
kuralme <kuralme@protonmail.com>
2025-08-29 06:41:20 +0000

    added an ompl planner for constrained planning

--- ur3e_sim_moveit_config/launch/move_group.launch.py
+++ ur3e_sim_moveit_config/launch/move_group.launch.py
@@ -1,17 +1,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
-import os
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    moveit_config = MoveItConfigsBuilder("name", package_name="ur3e_sim_moveit_config").sensors_3d(
-            file_path=os.path.join(
-                get_package_share_directory("ur3e_sim_moveit_config"),
-                "config/sensors_3d.yaml",
-            )
-        ).to_moveit_configs()
+    moveit_config = (
+        MoveItConfigsBuilder("name", package_name="ur3e_sim_moveit_config")
+        .robot_description_semantic(file_path="config/name.srdf")
+        .sensors_3d(file_path="config/sensors_3d.yaml")
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
+        )
+        .to_moveit_configs()
+    )
         
     move_group_node = Node(
         package="moveit_ros_move_group",
@@ -19,10 +20,12 @@ def generate_launch_description():
         output="screen",
         parameters=[
             moveit_config.to_dict(),
-            {"trajectory_execution.allowed_execution_duration_scaling": 2.0,},
+            {"trajectory_execution.allowed_execution_duration_scaling": 10.0},
             {"publish_robot_description_semantic": True},
             {"use_sim_time": True},
         ],
+        arguments=["--ros-args", "--log-level", "info"],
+        # prefix=["xterm -e gdb -ex run --args"],
     )
     return LaunchDescription(
         [move_group_node]
