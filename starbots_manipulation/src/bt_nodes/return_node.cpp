67859610dc51c3d3189b94f7e3e1a71403304993
kuralme <kuralme@protonmail.com>
2025-10-31 09:46:32 +0000

    gripper closed for safe return

--- starbots_manipulation/src/bt_nodes/return_node.cpp
+++ starbots_manipulation/src/bt_nodes/return_node.cpp
@@ -5,13 +5,13 @@ Return::Return(const std::string &name, const BT::NodeConfig &config,
     : BT::SyncActionNode(name, config), robot_(robot) {}
 BT::PortsList Return::providedPorts() { return {}; }
 
-BT::NodeStatus Return::tick()
-{
+BT::NodeStatus Return::tick() {
   robot_->publishStatus("Returning to quick-pick position", "INFO");
+  robot_->executeGripperPlan("gripper_close");
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
 
   robot_->goal_poses_.clear();
-  if (!robot_->gotoPredefined("quick_pick"))
-  {
+  if (!robot_->gotoPredefined("quick_pick")) {
     robot_->move_group_robot_->stop();
     robot_->clearOrientationConstraints();
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
